d1d734f9dea557c61e09e77a15c1cb728075efd4
Nethmini799 <117433954+DreamyD20@users.noreply.github.com>
2025-11-20 10:54:26 +0530

    next config js edit

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
-/** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
-
-const nextConfig = {
-  output: "export",
-  basePath: isProd ? "/my_portfolio" : "",
-  trailingSlash: true
-};
-
-module.exports = nextConfig;
+// next.config.js
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/old-path/:slug',
+        destination: '/new-path/:slug',
+      },
+    ]
+  },
+}
